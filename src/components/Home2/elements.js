1173b3a433ed9bf4b1c9c63dac6c45564109decd
Niko <huangnik90@gmail.com>
2026-06-02 16:36:58 +0700

    testing again 2

--- src/components/Home2/elements.js
+++ src/components/Home2/elements.js
@@ -449,7 +449,7 @@ export const DialogBox = styled.div`
   /* Mobile portrait (iPhone) */
   @media (max-width: 30rem) {
     bottom: calc(
-      11rem + env(safe-area-inset-bottom, 2rem)
+      21rem + env(safe-area-inset-bottom, 2rem)
     ); /* naikkan jadi 11rem + safe area */
     left: 0.75rem;
     right: 0.75rem;
